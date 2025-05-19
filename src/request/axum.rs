a358b4759ad2f7fdb68401c39a6a50f4e9311d56
baerwang <baerwang@apache.org>
2025-05-19 11:00:54 +0800

    feat: validator query of required fields

--- src/request/axum.rs
+++ src/request/axum.rs
@@ -19,7 +19,7 @@ use crate::request::validator::ValidateRequest;
 use anyhow::{Context, Result};
 use axum::body::{Body, Bytes};
 use axum::http::Request;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 
 #[allow(dead_code)]
@@ -64,6 +64,8 @@ impl ValidateRequest for RequestData {
             .into_owned()
             .collect();
 
+        let mut requireds: HashSet<String> = HashSet::new();
+
         if let Some(parameters) = &path_base.parameters {
             for parameter in parameters {
                 if parameter._in != In::Query {
@@ -74,9 +76,31 @@ impl ValidateRequest for RequestData {
                     validate_format(&parameter.schema.format, value, &parameter.name)?;
                 }
 
-                if let Some(schema_ref) = &parameter.schema._ref {
+                let mut refs = Vec::new();
+                if let Some(r) = &parameter.schema._ref {
+                    refs.push(r.as_str());
+                }
+                if let Some(one_of) = &parameter.schema.one_of {
+                    for s in one_of {
+                        if let Some(r) = &s._ref {
+                            refs.push(r.as_str());
+                        }
+                    }
+                }
+                if let Some(all_of) = &parameter.schema.all_of {
+                    for s in all_of {
+                        if let Some(r) = &s._ref {
+                            refs.push(r.as_str());
+                        }
+                    }
+                }
+
+                for schema_ref in refs {
                     if let Some(components) = &open_api.components {
-                        if let Some(schema) = components.schemas.get(schema_ref.as_str()) {
+                        if let Some(schema) = components.schemas.get(schema_ref) {
+                            if !schema.required.is_empty() {
+                                requireds.extend(schema.required.clone());
+                            }
                             if let Some(properties) = &schema.properties {
                                 for (key, prop) in properties {
                                     if let Some(value) = query_pairs.get(key) {
@@ -87,6 +111,15 @@ impl ValidateRequest for RequestData {
                         }
                     }
                 }
+            }
+        }
+
+        for key in &requireds {
+            if !query_pairs.contains_key(key) {
+                return Err(anyhow::anyhow!(
+                    "Missing required query parameter: '{}'",
+                    key
+                ));
             }
         }
 
